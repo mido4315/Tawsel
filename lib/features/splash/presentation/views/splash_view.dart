6cb48488736b16f5be6f982213c15991de3cb1d0
mido4315 <mohmed.mahmoud3@gmail.com>
2023-02-07 15:50:38 +0200

    test

--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -6,7 +6,8 @@ class SplashView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      body: Column(children: const [],),
+      body: Column(
+        children: const [],),
     );
   }
 }
